f8d4d35a412ffaca5c0d4b66447d156d3075d542
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-22 15:04:33 +0200

    init

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Google-faq</title>
+  <?php 
+    include 'data.php';
+  ?>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      Google Privacy e termini
+    </div>
+    <div class="nav__links">
+      <a href="#">Introduzione</a>
+      <a href="#">Norme sulla privacy</a>
+      <a href="#">Termini di servizio</a>
+      <a href="#">Tecnologie</a>
+      <a href="#">Domande frequenti</a>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <ul>
+        <li>
+          <h3>
+            Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?
+          </h3>
+          <p>
+            La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+          </p>
+        </li>
+      </ul>
+    </div>
+  </main>
+</body>
+</html>
