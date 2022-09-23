fa3b30e1cfb2767a26b5b02a10940ac8252a2146
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-23 09:19:48 +0200

    check

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800&family=Open+Sans:wght@300;400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800&family=Open+Sans:wght@300;400;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
   <title>Google-faq</title>
   <?php 
     include 'data.php';
@@ -16,14 +16,15 @@
 <body>
   <header>
     <div class="logo">
-      Google Privacy e termini
+      <img src="https://www.privacyacademy.it/wp-content/uploads/2021/03/google.png" alt="">
+      <span>Privacy & termini</span>
     </div>
     <div class="nav__links">
       <a href="#">Introduzione</a>
       <a href="#">Norme sulla privacy</a>
       <a href="#">Termini di servizio</a>
       <a href="#">Tecnologie</a>
-      <a href="#">Domande frequenti</a>
+      <a href="#" class="active__link">Domande frequenti</a>
     </div>
   </header>
   <main>
